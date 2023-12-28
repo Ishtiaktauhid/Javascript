f164b0d1a50f2c51fa0f2c798524599f33febc44
ishtiaktauhid <thouhid.islam2020@gmail.com>
2023-12-28 15:32:43 +0600

    jspractice_all

---
+++ javascript_practice/Chapter_11.js
@@ -0,0 +1,15 @@
+// let s = new Set(); // A new, empty set
+// let t = new Set([1, s]);
+// t.add(5)
+// t.delete(1); 
+// t.delete(5); 
+// console.log(t.size);
+
+// let str = "Hello  World!";
+// let pos = str.search("World");
+// console.log(pos); 
+let a = "JavaScript";
+  "JavaScript".search(/Script/ui);
+console.log("JavaScript");
+ 
+
